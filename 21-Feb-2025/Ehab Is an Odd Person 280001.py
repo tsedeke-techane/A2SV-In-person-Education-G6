f3cbcbf274b0c78c41eec0d0c37f7b63ba994ce8
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-02-21 23:58:55 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 21-Feb-2025/Ehab Is an Odd Person 280001.py
@@ -0,0 +1,14 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+oddNum = sum(1 for x in arr if x % 2 != 0)
+evenNum = n - oddNum
+
+if oddNum == 0 or evenNum == 0:
+    print(" ".join(map(str, arr)))
+else:
+    arr.sort()
+    print(" ".join(map(str, arr)))
+
