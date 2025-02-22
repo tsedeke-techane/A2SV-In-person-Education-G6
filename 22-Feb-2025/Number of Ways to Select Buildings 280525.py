cb95b23677e3005b95fc07ae44030898792efb1f
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-02-22 22:33:23 +0300

    Created 'Number of Ways to Select Buildings' by A2SV Hub

---
+++ 22-Feb-2025/Number of Ways to Select Buildings 280525.py
@@ -0,0 +1,19 @@
+# Problem: Number of Ways to Select Buildings - https://leetcode.com/problems/number-of-ways-to-select-buildings/
+
+class Solution:
+    def numberOfWays(self, s: str) -> int:
+        n = len(s)
+        zeros = s.count("0")
+        ones = n - zeros
+        left_zeros = left_ones = 0
+        ways = 0
+
+        for ch in s:
+            if ch == "0":
+                ways += left_ones * (ones - left_ones)
+                left_zeros += 1
+            else:
+                ways += left_zeros * (zeros - left_zeros)
+                left_ones += 1
+
+        return ways
