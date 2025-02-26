f3b86efeba844812c100d3b1f628e2164a58bfd5
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-02-26 18:06:30 +0300

    Created 'Partition Labels' by A2SV Hub

---
+++ 26-Feb-2025/Partition Labels 284977.py
@@ -0,0 +1,20 @@
+# Problem: Partition Labels - https://leetcode.com/problems/partition-labels/
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastIdx = {}
+        for i in range(len(s)):
+            lastIdx[s[i]] = i
+
+        print(lastIdx)
+        
+        curLast = 0
+        curStart = 0
+        ans = []
+        for i in range(len(s)):
+            curLast = max(curLast, lastIdx[s[i]])
+            if i == curLast:
+                ans.append(curLast - curStart + 1)
+                curStart = i + 1
+        
+        return ans
