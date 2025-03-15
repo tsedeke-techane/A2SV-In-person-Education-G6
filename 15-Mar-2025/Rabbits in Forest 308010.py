06967db824e9275cd3160a049a754105f4487937
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-03-15 23:05:03 +0300

    Created 'Rabbits in Forest' by A2SV Hub

---
+++ 15-Mar-2025/Rabbits in Forest 308010.py
@@ -0,0 +1,13 @@
+# Problem: Rabbits in Forest - https://leetcode.com/problems/rabbits-in-forest/
+
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        count = Counter(answers)
+        ans = 0
+        for rabbits, freq in count.items():
+            group_size = rabbits + 1 
+            groups = (freq + group_size - 1) // group_size  
+            ans += groups * group_size  
+
+        return ans
+
