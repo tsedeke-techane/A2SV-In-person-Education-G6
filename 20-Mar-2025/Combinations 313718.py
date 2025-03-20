a62e23557c4b658be2c527189ce29ff413ebf959
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-03-20 22:38:24 +0300

    Created 'Combinations' by A2SV Hub

---
+++ 20-Mar-2025/Combinations 313718.py
@@ -0,0 +1,23 @@
+# Problem: Combinations - https://leetcode.com/problems/combinations/
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(start):
+            if len(path) == k:
+                ans.append(path[:])
+                return 
+            if start > n:
+                return
+
+            path.append(start)
+            backtrack(start + 1)
+
+            path.pop()
+            backtrack(start + 1)
+
+        ans = []
+        path = []
+        backtrack(1)
+        return ans
+                
+            
