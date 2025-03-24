07eaa6711e6e8f1a3f6416d4f7414ad9b935318c
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-03-24 16:30:14 +0300

    Created 'C - Penalty Shootout' by A2SV Hub

---
+++ A2SV G6 - Round #7 24-Mar-2025/C - Penalty Shootout 316712.py
@@ -0,0 +1,51 @@
+# Problem: C - Penalty Shootout - https://codeforces.com/gym/596141/problem/C
+
+import sys
+from math import ceil
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+def earliest_decision_point(s):
+    length = len(s)
+    ans = length
+
+    def recurse(index, cur1, cur2):
+        nonlocal ans
+        if index >= length:
+            return
+
+        remaining = length - index
+        if index % 2:
+            if cur2 + ceil(remaining / 2) < cur1 or cur1 + remaining // 2 < cur2:
+                ans = min(ans, index)
+                return
+            if s[index] == "1":
+                recurse(index + 1, cur1, cur2 + 1)
+            elif s[index] == "0":
+                recurse(index + 1, cur1, cur2)
+            else:
+                recurse(index + 1, cur1, cur2 + 1)
+                recurse(index + 1, cur1, cur2)
+        else:
+            if cur1 + ceil(remaining / 2) < cur2 or cur2 + remaining // 2 < cur1:
+                ans = min(ans, index)
+                return
+            if s[index] == "1":
+                recurse(index + 1, cur1 + 1, cur2)
+            elif s[index] == "0":
+                recurse(index + 1, cur1, cur2)
+            else:
+                recurse(index + 1, cur1 + 1, cur2)
+                recurse(index + 1, cur1, cur2)
+
+    recurse(0, 0, 0)
+    return ans
+
+
+t = int(input().strip())
+for _ in range(t):
+    s = input().strip()
+    print(earliest_decision_point(s))
