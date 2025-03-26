100d107da300d03913ba2ea296f8c3020c6b22c5
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-03-26 11:52:39 -0400

    Created 'Sqrt(x)' by A2SV Hub

---
+++ 26-Mar-2025/Sqrt(x) 319005.py
@@ -0,0 +1,20 @@
+# Problem: Sqrt(x) - https://leetcode.com/problems/sqrtx/
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        ans = 0
+        left = 0
+        right = x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+                 
+            elif mid * mid < x:
+                ans = mid
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return ans
