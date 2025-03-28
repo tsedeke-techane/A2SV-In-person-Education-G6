44224fc76d05e56d0305a830fb78644af9ca9e7d
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-03-28 15:23:31 -0400

    Created 'Minimum Time to Repair Cars' by A2SV Hub

---
+++ 28-Mar-2025/Minimum Time to Repair Cars 321258.py
@@ -0,0 +1,24 @@
+# Problem: Minimum Time to Repair Cars - https://leetcode.com/problems/minimum-time-to-repair-cars/
+
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        def count_repaired(time):
+            count = 0
+            for rank in ranks:
+                count += int(sqrt(time / rank)) 
+            return count
+        
+        left, right = 1, ranks[0] * cars * cars
+        ans = -1
+
+        while left <= right:
+            mid = (left + right) // 2
+            repaired = count_repaired(mid)  
+            
+            if repaired >= cars:  
+                ans = mid
+                right = mid - 1
+            else:  
+                left = mid + 1
+
+        return ans  
