3fe752914bcf6cc8501a906725c5f4ab79ede757
Tsedeke Techane <164669795+tsedeke-techane@users.noreply.github.com>
2025-03-31 23:34:39 +0300

    Created 'Lowest Common Ancestor of a Binary Search Tree' by A2SV Hub

---
+++ 31-Mar-2025/Lowest Common Ancestor of a Binary Search Tree 322923.py
@@ -0,0 +1,21 @@
+# Problem: Lowest Common Ancestor of a Binary Search Tree - https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        curr = root
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+
+            else:
+                return curr
